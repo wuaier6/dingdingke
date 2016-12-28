16b6036325d7f22c5cb0bae8bdd743d74ce74b7a
Shen Tao <shentao@OSKSH.GC.OMRON.NET>
2016-12-28 21:35:04 +0800

    功能修正

---
+++ resources/views/console/student/info.blade.php
@@ -0,0 +1,190 @@
+@extends('adminlayouts.app')
+
+@section('title', 'Moell Blog')
+
+@section('style')
+        <!-- Theme style -->
+<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap-datetimepicker/4.17.43/css/bootstrap-datetimepicker.css">
+<link rel="stylesheet" href="/packages/admin/AdminLTE/plugins/select2/select2.min.css">
+
+
+@endsection
+
+@section('content')
+        <!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        学生详细
+        <small>欢迎使用订课通</small>
+    </h1>
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+
+    <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">当月准时上课数</span>
+                    <span class="info-box-number">90<small>%</small></span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Unpass次数</span>
+                    <span class="info-box-number">41,410</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <!-- fix for small devices only -->
+        <div class="clearfix visible-sm-block"></div>
+
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">pass</span>
+                    <span class="info-box-number">760</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">New Members</span>
+                    <span class="info-box-number">2,000</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+    </div>
+    <div class="row">
+        <div class="col-sm-3">
+        <div class="box box-primary">
+            <div class="box-body box-profile">
+                <img class="profile-user-img img-responsive img-circle" src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
+
+                <h3 class="profile-username text-center">姓名</h3>
+
+                <p class="text-muted text-center">出生年月</p>
+
+                <ul class="list-group list-group-unbordered">
+                    <li class="list-group-item">
+                        <b>学校</b> <a class="pull-right">嘉善小学</a>
+                    </li>
+                    <li class="list-group-item">
+                        <b>班主任</b> <a class="pull-right">xxx老师</a>
+                    </li>
+                    <li class="list-group-item">
+                        <b>班级</b> <a class="pull-right">5年一般</a>
+                    </li>
+                </ul>
+                <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+            </div>
+            <!-- /.box-body -->
+        </div>
+        </div>
+        <div class="col-sm-9">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">最近上课评价</h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <ul class="products-list product-list-in-box">
+                        <li class="item">
+                            <div class="product-img">
+                                <img src="dist/img/default-50x50.gif" alt="Product Image">
+                            </div>
+                            <div class="product-info">
+                                <a href="javascript:void(0)" class="product-title">Samsung TV
+                                    <span class="label label-warning pull-right">$1800</span></a>
+                        <span class="product-description">
+                          Samsung 32" 1080p 60Hz LED Smart HDTV.
+                        </span>
+                            </div>
+                        </li>
+                        <!-- /.item -->
+                        <li class="item">
+                            <div class="product-img">
+                                <img src="dist/img/default-50x50.gif" alt="Product Image">
+                            </div>
+                            <div class="product-info">
+                                <a href="javascript:void(0)" class="product-title">Bicycle
+                                    <span class="label label-info pull-right">$700</span></a>
+                        <span class="product-description">
+                          26" Mongoose Dolomite Men's 7-speed, Navy Blue.
+                        </span>
+                            </div>
+                        </li>
+                        <!-- /.item -->
+                        <li class="item">
+                            <div class="product-img">
+                                <img src="dist/img/default-50x50.gif" alt="Product Image">
+                            </div>
+                            <div class="product-info">
+                                <a href="javascript:void(0)" class="product-title">Xbox One <span class="label label-danger pull-right">$350</span></a>
+                        <span class="product-description">
+                          Xbox One Console Bundle with Halo Master Chief Collection.
+                        </span>
+                            </div>
+                        </li>
+                        <!-- /.item -->
+                        <li class="item">
+                            <div class="product-img">
+                                <img src="dist/img/default-50x50.gif" alt="Product Image">
+                            </div>
+                            <div class="product-info">
+                                <a href="javascript:void(0)" class="product-title">PlayStation 4
+                                    <span class="label label-success pull-right">$399</span></a>
+                        <span class="product-description">
+                          PlayStation 4 500GB Console (PS4)
+                        </span>
+                            </div>
+                        </li>
+                        <!-- /.item -->
+                    </ul>
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer text-center">
+                    <a href="javascript:void(0)" class="uppercase">View All Products</a>
+                </div>
+                <!-- /.box-footer -->
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+@endsection
+
+@section('script')
+    <script src="/packages/admin/AdminLTE/plugins/select2/select2.full.min.js"></script>
+    <script src="/packages/admin/AdminLTE/plugins/iCheck/icheck.min.js"></script>
+    <script src="//cdn.bootcss.com/moment.js/2.8.1/moment.min.js"></script>
+    <script src="//cdn.bootcss.com/bootstrap-datetimepicker/4.17.43/js/bootstrap-datetimepicker.min.js"></script>
+@endsection
---
+++ resources/views/console/student/list.blade.php
@@ -0,0 +1,77 @@
+@extends('adminlayouts.app')
+
+@section('title', 'Moell Blog')
+
+@section('style')
+
+
+@endsection
+
+@section('content')
+        <!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        老师列表
+        <small>欢迎使用订课通</small>
+    </h1>
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <div class="box box-default">
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title"></h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-footer">
+                <ul class="mailbox-attachments clearfix">
+
+                    @foreach($teacher_list as $teacher_val)
+                    <li>
+                        <span class="mailbox-attachment-icon has-img"><img
+                                    src="{{$teacher_val->headpic}}"
+                                    alt=""></span>
+                        <div class="mailbox-attachment-info">
+                            <div>
+                                <a href="/teacher/edit/{{$teacher_val->id}}" class="mailbox-attachment-name" style="word-break:break-all;"><i
+                                            class="fa fa-pencil"></i>&nbsp;&nbsp;{{$teacher_val->name}}
+                                </a>
+                            </div>
+                            <div>
+                                <i class="fa fa-camera"></i>&nbsp;&nbsp;1992-12-12
+                            </div>
+                            <div>
+                                <i class="fa fa-phone"></i>&nbsp;&nbsp;{{$teacher_val->cell}}
+                            </div>
+                            {{--<span class="mailbox-attachment-size">--}}
+                            {{--<input type="checkbox" class="grid-item" data-id="test"/>--}}
+                            {{--<span class="pull-right">--}}
+                                    {{--<a href="" target="_blank" download="custom-filename.jpg"><i--}}
+                                                {{--class="fa fa-cloud-download"></i></a>--}}
+                            {{--</span>--}}
+                            {{--</span>--}}
+                        </div>
+                    </li>
+                    @endforeach
+
+                </ul>
+            </div>
+
+            <div class="box-footer clearfix">
+
+            </div>
+            <!-- /.box-body -->
+        </div>
+    </div>
+
+
+</section>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+@endsection
+
+@section('script')
+
+@endsection
